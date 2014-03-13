f1a4bcca05caed8037241dbe29ac325f3dae51c9
bloot <bloot@users.noreply.github.com>
2014-03-13 06:21:10 -0400

    Indent 2 spaces instead of 4

--- lib/classes/holocaust.rb
+++ lib/classes/holocaust.rb
@@ -1,44 +1,44 @@
 class Holocaust
 
-	def initialize(username, password)
-		@gbot = Snooby::Client.new("#{username}, 0.1")
-		@gbot.authorize!(username, password)
-		dictionary = YAML.load_file(File.expand_path('../data/dictionary.yaml'))
-		@dictionary = dictionary[:words]
-		read_com
-	end
+  def initialize(username, password)
+    @gbot = Snooby::Client.new("#{username}, 0.1")
+    @gbot.authorize!(username, password)
+    dictionary = YAML.load_file(File.expand_path('../data/dictionary.yaml'))
+    @dictionary = dictionary[:words]
+    read_com
+  end
 
-	def read_com
-		while true
-		  @gbot.r('all').comments.each do |com|
-		  	sr = com['subreddit']
-			  	@com = com
-			  	comment = com.body.to_s
-			  	check_com(comment)
-			end	
-		  end
-		end
-	end
+  def read_com
+    while true
+      @gbot.r('all').comments.each do |com|
+        sr = com['subreddit']
+        @com = com
+        comment = com.body.to_s
+        check_com(comment)
+      end 
+      end
+    end
+  end
 
-	def check_com(comment)
-		mis_words = []
-		comment = comment.split
-		@dictionary.each do |misp, word|
-			if comment.include?(misp)
-				mis_words = mis_words << word
-			end
-		end
-		correct_comment(mis_words)
-	end
+  def check_com(comment)
+    mis_words = []
+      comment = comment.split
+      @dictionary.each do |misp, word|
+        if comment.include?(misp)
+          mis_words = mis_words << word
+        end
+      end
+    correct_comment(mis_words)
+  end
 
-	def correct_com(words)
-		if words.size > 1
-			@com.reply("EDIT: words.. #{words.join("*, ")}*")
-			puts @com.body
-		elsif words.size == 1
-			@com.reply("EDIT: word.. #{words[0]}*")
-			puts @com.body
-		end
-	end
+  def correct_com(words)
+    if words.size > 1
+      @com.reply("EDIT: words.. #{words.join("*, ")}*")
+      puts @com.body
+    elsif words.size == 1
+      @com.reply("EDIT: word.. #{words[0]}*")
+      puts @com.body
+    end
+  end
 
 end
